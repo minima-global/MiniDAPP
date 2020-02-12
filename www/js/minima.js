bb264692db7f65e8aee6947b940db0255d1eadc7
spartacus <spartacus@localhost>
2020-02-12 10:02:47 +0000

    Rejig code to create a Minima object

--- www/js/minima.js
+++ www/js/minima.js
@@ -5,59 +5,78 @@
 * 
 */
 
-/**
- * The RPC host 
- */
-var RPCHOST = "";
-
-//The UUID
-var UUID = Math.floor(Math.random()*1000000000);
-
-var initText = "<center><h3>Minima - MiFi</h3>" + 
-window.location.host+" would like to access the Minima network via your phone.<br>" + 
-"<br>" +
-"To get started you must first link your phone to this webpage<br>" + 
-"<br>" +
-"Your phone and this computer must be running on the same WiFi<br>" +
-"<br>"+
-"To continue.. press<br>" + 
-"<br>" + 
-"<button onclick='stage2();'>Next</button><br>" +
-//"<br>" +
-//"If this webpage is actually ON your phone..<br>" +
-//"<br>" +
-//"<button onclick='stageLink();'>Local Link</button><br>" + 
-"</center>";
-
-var stageTwo = "<center><h3>Minima - MiFi</h3>\n" + 
-"<center>" +
-"<div id='miniqrcode' style='width:200px; height:200px; margin-top:15px;'></div>"+
-"<br>" + 
-"Open the Minnma app and choose <b>Weblink</b><br>" + 
-"<br>" + 
-"OR<br><br>" +
-"Open the <b>Terminal</b> and type :<br><br>" +
-"weblink " + UUID+ 
-"</center>";
-
-/**
- * Initialise access to Minima
- * 
- * @param callback
- * @returns
- */
-function MinimaStart(startcallback){
-	//Log a little..
-	console.log("Minima : Initialisation started..");
+var Minima = {
+	status : "zero",	
+	blocknumber : 0,
+	host : "0.0.0.0",
+	uuid : Math.floor(Math.random()*1000000000),
+	logging : true,
 	
-	//Do we already have an IP to talk to..
-//	var ip = window.localStorage.getItem("phoneip");
-//	if(ip == null){
-//		ip = "none";
-//	}else{
-//		//Check if this connection still valid..
-//		console.log("Minima : Previouis settings found.. testing..");
-//	}
+	//Minima Startup
+	init : function(){
+		//Log a little..
+		log("Initialisation started..");
+		
+		postMessage("init", "");
+		
+		//Show the Overlay divs
+		showOverlayDivs();
+		
+		//Now start the Websocket
+		startWebSocket();
+	},
+	
+	//Runs a function on the phone
+	cmd : function(minifunc, callback){
+		//Create the string..
+		var rpc = "http://"+Minima.host+"/"+minifunc;
+
+		//log it..
+		log("RPC call "+rpc);
+		
+		//And Call it..
+		httpGetAsync(rpc, callback);
+	},
+	
+	//Wipes the Locally stored details of the phone IP
+	logout : function(){}
+};
+
+/**
+ * Utility functions used by the Minima Object
+ * 
+ */
+function log(info){
+	if(Minima.logging){
+		console.log("Minima : "+info);
+	}
+}
+
+function setStatus(status){
+	log("Status changed to "+status);
+	Minima.status = status;
+}
+
+function postMessage(event, info){
+   log("Event Dispatch "+event+" "+info);
+	
+   //And finally..
+   var data = { "event": event, "info" : info }
+   
+   //And dispatch
+   var custom = new CustomEvent("MinimaEvent", {detail:data});
+   window.dispatchEvent(custom);
+}
+
+function showOverlayDivs(){
+	var initText = "<center><h3>Minima - MiFi</h3>" + 
+	window.location.host+" would like to access the Minima network via your phone.<br><br>" +
+	"To get started you must first link your phone to this webpage<br><br>" +
+	"Your phone and this computer must be running on the same WiFi<br><br>"+
+	"To continue.. press<br><br>" + 
+	"<button onclick='stage2();'>Proceed</button><br>" +
+	"</center>";
+
 	
 	//First add the total overlay div
 	var overdiv = document.createElement('div');
@@ -71,77 +90,79 @@ function MinimaStart(startcallback){
 	div.id = "MinimaDIV";
 	div.innerHTML = initText;
 	document.body.appendChild(div);
+}
 
+function hideOverLayDivs(){
+	//Hide the Divs..
+   document.getElementById("MinimaDIV").style.display = "none";
+   document.getElementById("MinimaOverlay").style.display = "none";
+}
+
+function startWebSocket(){
 	//Open up a websocket to the main MINIMA proxy..
 	var ws = new WebSocket("ws://127.0.0.1:8889");
 	
 	ws.onopen = function() {
-		console.log("WS Connection opened to the Minima Proxy..");
+	   log("WS Connection opened to the Minima Proxy..");
 		
-		// Web Socket is connected, send data using send()
-	   ws.send(UUID);
+	   // Web Socket is connected, send data using send()
+	   ws.send(Minima.uuid);
+	   
+	   //Change the Status
+	   setStatus("uuid_sent");
 	};
 	
 	ws.onmessage = function (evt) { 
-	   console.log("WS message received : "+evt.data);
-	   
 	   //Now that we have the IP.. Set it.. 
-	   window.localStorage.setItem('phoneip', evt.data);
+	   window.localStorage.setItem('MinimaIP', evt.data);
 	   
 	   //Set it..
-	   RPCHOST = evt.data;
+	   Minima.host = evt.data;
+	   log("Host set "+Minima.host);
 	   
 	   //And close
 	   ws.close();
 	   
 	   //Hide the Divs..
-	   document.getElementById("MinimaDIV").style.display = "none";
-	   document.getElementById("MinimaOverlay").style.display = "none";
+	   hideOverLayDivs();
 	   
-	   startcallback();
+	   //Should be good..
+	   setStatus("connected");
 	   
-//	   //And finally..
-//	   var data = { "event":"startup" }
-//	   
-//	   //And dispatch
-//	   var event = new CustomEvent('MinimaEvent', {detail:data});
-//	   window.dispatchEvent(event);
+	   //Send a message
+	   postMessage("connected", "success")
 	};
 		
-	ws.onclose = function() { console.log("Connection is closed..."); };
+	ws.onclose = function() { 
+		log("WS Connection is closed...");
+	};
 
 	ws.onerror = function(error) {
 		//var err = JSON.stringify(error);
 		var err = JSON.stringify(error, ["message", "arguments", "type", "name", "data"])
-		
-	    // websocket is closed.
-	   console.log("Error ... "+err); 
+		// websocket is closed.
+	    log("WS Error ... "+err); 
 	};
 }
 
 function stage2(){
-	var md = document.getElementById("MinimaDIV");
+	var stageTwo = "<center><h3>Minima - MiFi</h3>\n" + 
+	"<div id='miniqrcode' style='width:200px; height:200px; margin-top:15px;'></div><br>" + 
+	"Open the Minnma app and choose <b>Weblink</b><br><br>" + 
+	"OR<br><br>" +
+	"Open the <b>Terminal</b> and type :<br><br>weblink "+Minima.uuid+ 
+	"</center>";
 	
-	md.innerHTML = stageTwo;
+	//Set the Text
+	document.getElementById("MinimaDIV").innerHTML = stageTwo;
 	
 	//Create the QR Code
 	var qrcode = new QRCode(document.getElementById("miniqrcode"), {
 		width : 200,
 		height : 200
 	});
-	qrcode.makeCode(UUID);
-	
-//	window.localStorage.setItem('phoneip', 'Time : '+new Date());
+	qrcode.makeCode(Minima.uuid);
 }
-
-function MinimaCMD(zRPCCommand, callback){
-	//Create the string..
-	var rpc = "http://"+RPCHOST+"/"+zRPCCommand;
-	
-	//And Call it..
-	httpGetAsync(rpc, callback);
-}
-
 
 /**
  * Utility function for GET request
