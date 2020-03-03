0a21d26baa49eca63217e886b0f4250d4fce3624
spartacusrex <patrickcerri@gmail.com>
2020-03-03 12:03:28 +0000

    New vault address started.. scriptide required to clean complex scripts

--- paste.java
+++ paste.java
@@ -7,3 +7,15 @@
 "		<td style='text-align:left;vertical-align:middle;'>MIFI LOGOUT</td>\n" + 
 "	</tr>\n" + 
 "</table>"
+
+"LET pkcold = 0x876876876<br>\n" + 
+"IF SIGNEDBY pkcold THEN RETURN TRUE ENDIF<br>\n" + 
+"LET pkhot = 0x75765765765<br>\n" + 
+"LET amt = GETSTATE ( 20 )<br>\n" + 
+"LET recip = GETSTATE ( 21 )<br>\n" + 
+"LET root = CONCAT ( [ IF SIGNEDBY ] SCRIPT ( pkcold ) [ THEN RETURN TRUE ENDIF ] )<br>\n" + 
+"LET user = CONCAT ( [ IF SIGNEDBY ] SCRIPT ( recip ) [ AND ( @BLKNUM - @INBLKNUM ) GT 20 THEN RETURN TRUE ENDIF ] )<br>\n" + 
+"LET final = CONCAT ( root user )<br>\n" + 
+"ASSERT VERIFYOUT ( @INPUT SHA3 ( final ) amt @TOKENID )<br>\n" + 
+"ASSERT VERIFYOUT ( ( @INPUT + 1 ) @ADDRESS ( @AMOUNT - amt ) @TOKENID )<br>\n" + 
+"IF SIGNEDBY pkhot THEN RETURN TRUE ENDIF<br>"
