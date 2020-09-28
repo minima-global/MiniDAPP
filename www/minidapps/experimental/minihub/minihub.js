1e0afbf6dc77721bfd04713432c81b50149d78ae
spartacusrex <patrickcerri@gmail.com>
2020-09-28 11:38:12 +0100

    testing new minihub

---
+++ www/minidapps/experimental/minihub/minihub.js
@@ -0,0 +1,39 @@
+
+
+function makeMenu(minidapps){
+
+	int dapplen = minidapps.length;
+	
+	
+
+	
+
+
+
+
+}
+
+
+function outputDAPP(minidapp){
+	
+	var html = "<table width=100%><tr><td>"
+			+"<table style='background-size:100%;background-image: url(/minidapps/coinflip/./images/backdrop.png);' width=100% height=100 class=minidapp>
+			 	<tr>
+					<td style='cursor:pointer;' rowspan=2 onclick="window.open('/minidapps/coinflip/index.html', '_Coin Flip');">
+						<img src='/minidapps/coinflip/./images/coinicon.png' height=100>
+					</td>
+					<td width=100% class='minidappdescription'>
+                   <div style='position:relative'>
+				        <div style='color:red;position:absolute;right:10;top:10'>DEVELOPMENT</div>
+						<br><div onclick="window.open('/minidapps/coinflip/index.html','_Coin Flip');" style='cursor:pointer;font-size:18'><b>COIN FLIP</b></div>
+						<br><div onclick="window.open('/minidapps/coinflip/index.html','_Coin Flip');" style='cursor:pointer;font-size:12;min-height:20;'>50 / 50 Gambler.. 1 on 1.. just need to find a worthy opponent..</div>
+						<div onclick="window.open('/minidapps/coinflip/index.html','_Coin Flip');" style='cursor:pointer;color:blue;font-size:10;text-align:right;width:98%;'><br>coinflip @ 25/09/2020 10:21:50</div>
+					</div>
+                     </td>
+				</tr>
+			</table>
+		</td></tr>
+		</table>";
+	
+	
+}
