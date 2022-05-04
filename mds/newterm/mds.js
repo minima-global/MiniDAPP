ec2dd7029f9addaec52898ebc4d40cbda45ed13a
spartacusrex <patrickcerri@gmail.com>
2022-05-04 09:48:37 +0100

    new mds.js with normal cmd and cmdjson

--- mds/newterm/mds.js
+++ mds/newterm/mds.js
@@ -62,14 +62,35 @@ var MDS = {
 	},
 	
 	/**
-	 * Runs a function on the Minima Command Line
+	 * Runs a function on the Minima Command Line - same format as MInima
 	 */
 	cmd : function(command, callback){
+		//Convert to a JSON object for MDS
+		var jsonobj = splitLineToJSON(command);
+		
 		//Send via POST to MDS
-		httpPostAsync(MDS.rpchost, command, callback);
+		MDS.cmdjson(jsonobj,callback);
+	},
+	
+	/**
+	 * Runs a function on the Minima Command Line - using a JSON object
+	 *
+     * the format is { "name":"the_command", "args":{..} }
+	 */
+	cmdjson : function(commandjson, callback){
+		//Convert JSON to a string..
+		var datastr = JSON.stringify(commandjson) 
+		
+		console.log("command : "+datastr);
+		
+		//Send via POST to MDS
+		httpPostAsync(MDS.rpchost, datastr, callback);
 	},
 		
-		
+	/**
+	 * Run SQL - just provide the sql command string
+     * Will return a JSON object if a SELECT
+	 */	
 	sql : function(sqlcommand, callback){
 		//create 
 		sqlcmd = {};
@@ -221,4 +242,54 @@ function httpGetAsync(theUrl, callback, logenabled)
     }
     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
     xmlHttp.send(null);
+}
+
+/**
+ * Split line into a JSON object
+ */
+function splitLineToJSON(commandline){
+
+	//Split the line up - keeping the quoted areas..
+	words = commandline.match(/(?:[^\s"']+|['"][^'"]*["'])+/g);
+	
+	//The function is the FIRST
+	name = words[0];
+	
+	//The final JSON
+	var jsoncommand  = {};
+	
+	//The main function is always first
+	jsoncommand.name = name;
+	
+	//The arguments
+	var args = {};
+	for (var i = 0; i < words.length; i++) { 
+		
+		//Get the word
+		word = words[i].trim();
+		
+		//Now break it down..
+		if(word!=""){
+			
+			//remove the quotes if any
+			if(word.startsWith('"') || word.endsWith("'")){
+				word = word.substring(1);
+			}
+			
+			if(word.endsWith('"') || word.endsWith("'")){
+				word = word.substring(0,word.length-1);
+			}
+			
+			//Now split with the :
+			splitter = word.split(":");
+			
+			//Add to the json
+			args[splitter[0]] = splitter[1];
+		}
+	}
+	
+	//Now add the args
+	jsoncommand.args = args;
+	
+	return jsoncommand;
 }
